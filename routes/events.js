e17cfe2dccd0647d618bbe078fcf947f70353f09
Ilya Mikhaylov <doxxmsk@gmail.com>
2019-12-20 10:58:29 +0300

    Editor fix

--- routes/events.js
+++ routes/events.js
@@ -15,12 +15,13 @@ const saltRounds = 10;
 router.get('/events', async function(req, res, next) {
     try {
         // const date1 = Date.
-        const findDate = await Event.find({activity: 'Маникюр'});
-        await console.log(findDate[0].firstDate);
-        const date1 = await findDate[0].firstDate;
-        await console.log(date1.getDate());
+        // const findDate = await Event.find({activity: 'Маникюр'});
+        // await console.log(findDate[0].firstDate);
+        // const date1 = await findDate[0].firstDate;
+        // await console.log(date1.getDate());
         const events = await Event.find({user: req.session.user._id}); /// Что мы получаем из кук? TODO: Отфильтровать по месяцу, категориям,
-        res.render('events', {events}); /// TODO: Обновить календарь
+        console.log(events)
+        res.render('events', {events: events, title: 'Твои активности'}); /// TODO: Обновить календарь
     } catch(e) {
         console.log(e)
     }
@@ -28,7 +29,12 @@ router.get('/events', async function(req, res, next) {
 
 router.get('/events/new', async function(req, res, next) {
     try {
-        res.render('editor');
+        res.render('editor', {
+            title: 'Новая задача',
+            header: 'Новая задача',
+            method: 'post',
+            action: 'Создать',
+        });
     } catch(e) {
         console.log(e);
     }
@@ -56,8 +62,30 @@ router.post('/events', async function(req, res, next) {
     }
 });
 
-router.get('/events:id', function(req, res, next) {
-    res.render('event');
+router.get('/events/:id', async function(req, res, next) {
+    try {
+        const event = await Event.find({_id: req.params._id});
+        function specialist(e) {
+            if (e === true) return 'Да';
+            else return 'Нет'
+        }
+        await console.log(event.period);
+        res.render('editor', {
+            title: 'Редактировать задачу',
+            header: 'Редактировать задачу',
+            method: 'put',
+            action: 'Редактировать',
+            event: event,
+            activity: event.activity,
+            firstDate: event.firstDate,
+            period: await event.period,
+            notifyBefore: event.notifyBefore,
+            specialist: specialist(event.specialist),
+            cost: event.cost,
+        });
+    } catch(e) {
+        console.log(e);
+    }
 });
 
 router.put('/events:id', async function(req, res, next) {
@@ -81,7 +109,7 @@ router.delete('/events/:id', async function(req, res, next) {
     try {
         if (req.session.user) {
             await Event.deleteOne({'_id': req.params._id});
-            res.redirect('/events');
+            // res.redirect('/events');
         } else {
             res.render('error', {message: 'Unauthorized operation'})
         }
